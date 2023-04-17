36adec1c75c63ee2d3ae88662e235f7ca05fae27
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2023-04-17 23:56:16 +0330

    feat: image upload to cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
   CATEGORY,
@@ -51,6 +52,7 @@ export default function RentModal() {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(
     () => dynamic(() => import('../Map'), { ssr: false }),
@@ -151,6 +153,21 @@ export default function RentModal() {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={value => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={value => setCustomValue('imageSrc', value)}
         />
       </div>
     );
